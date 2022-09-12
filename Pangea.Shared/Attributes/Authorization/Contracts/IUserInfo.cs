f3c8033c18d49cd2b9d0bb26d6fd5b892d63850c
Nika Julakidze <nikaj@pangea-it.com>
2022-09-12 15:46:32 +0400

    Added aditional properties to IuserInfo interface.

--- Pangea.Shared/Attributes/Authorization/Contracts/IUserInfo.cs
+++ Pangea.Shared/Attributes/Authorization/Contracts/IUserInfo.cs
@@ -5,6 +5,8 @@
         string? UserName { get; }
         string? SecurityStamp { get; }
         string? UserId { get; }
+        string? ClientIp { get; }
+        string? ClientDevice { get; }
         IEnumerable<string> Claims { get; }
 
         bool HasClaim(string claim);
