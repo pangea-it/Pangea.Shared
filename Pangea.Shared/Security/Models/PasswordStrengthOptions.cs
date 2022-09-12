00b3160e91d0070076700071d3299102d0114ffb
GioNatro2022 <giorgin@pangea-it.com>
2022-09-12 14:46:45 +0400

    password genrator service

---
+++ Pangea.Shared/Security/Models/PasswordStrengthOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pangea.Shared.Security.Models
+{
+    public class PasswordStrengthOptions
+    {
+        public int RequiredLength { get; set; }
+        public int RequiredUniqueChars { get; set; }
+        public bool RequireDigit { get; set; }
+        public bool RequireLowercase { get; set; }
+        public bool RequireNonAlphanumeric { get; set; }
+        public bool RequireUppercase { get; set; }
+    }
+}
