94dcfbc9dc0ce10370ae130c47046948233c8736
GioNatro2022 <giorgin@pangea-it.com>
2022-09-22 17:28:12 +0400

    file extensions

---
+++ Pangea.Shared/Extensions/FormFileExtensions/FormFileExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Pangea.Shared.Extensions.FormFileExtensions
+{
+    public static class FormFileExtensions
+    {
+
+        public static async Task<byte[]> GetBytes(this IFormFile formFile)
+        {
+            await using var memoryStream = new MemoryStream();
+            await formFile.CopyToAsync(memoryStream);
+            return memoryStream.ToArray();
+        }
+    }
+}
